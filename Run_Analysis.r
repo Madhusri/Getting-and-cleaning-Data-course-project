e8dcd0521421ad38b6bae265ffd3a7051325d87e
Madhusri <Madhusri@users.noreply.github.com>
2014-09-13 17:17:31 -0700

    Update Run_Analysis.r

--- Run_Analysis.r
+++ Run_Analysis.r
@@ -60,28 +60,28 @@ colNames  = colnames(finalData);
 **2. Extract only the measurements on the mean and standard deviation for each measurement.**
 
  
-# Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others 
+# Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others. 
  logicalVector = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames)); 
 
  
-# Subset finalData table based on the logicalVector to keep only desired columns 
+# Subset finalData table based on the logicalVector to keep only desired columns. 
 finalData = finalData[logicalVector==TRUE]; 
 
  
 ** 3. Use descriptive activity names to name the activities in the data set** 
  
- # Merge the finalData set with the acitivityType table to include descriptive activity names 
+ # Merge the finalData set with the acitivityType table to include descriptive activity names. 
  finalData = merge(finalData,activityType,by='activityId',all.x=TRUE); 
 
 
- # Updating the colNames vector to include the new column names after merge 
+ # Updating the colNames vector to include the new column names after merge .
  colNames  = colnames(finalData);  
  
  
 ** 4. Appropriately label the data set with descriptive activity names.**  
 
  
-# Cleaning up the variable names 
+# Cleaning up the variable names. 
 
 for (i in 1:length(colNames))  
  { 
@@ -100,13 +100,13 @@ for (i in 1:length(colNames))
  }; 
  
  
-# Reassigning the new descriptive column names to the finalData set 
+# Reassigning the new descriptive column names to the finalData set.
 colnames(finalData) = colNames; 
  
  
 ** 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject.**  
  
- # Create a new table, finalDataNoActivityType without the activityType column 
+ # Create a new table, finalDataNoActivityType without the activityType column. 
  finalDataNoActivityType  = finalData[,names(finalData) != 'activityType']; 
  
  
@@ -114,9 +114,9 @@ colnames(finalData) = colNames;
 tidyData    = aggregate(finalDataNoActivityType[,names(finalDataNoActivityType) != c('activityId','subjectId')],by=list(activityId=finalDataNoActivityType$activityId,subjectId = finalDataNoActivityType$subjectId),mean); 
  
  
-# Merging the tidyData with activityType to include descriptive acitvity names 
+# Merging the tidyData with activityType to include descriptive acitvity names. 
 tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE); 
  
  
-# Export the tidyData set  
+# Export the tidyData set. 
 write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t'); 
